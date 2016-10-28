e111454cb9bcab91c11633a15014ddca583a1eba
dnguyen2015 <dnguyen2015@fau.edu>
2016-10-27 23:43:37 -0400

    FCFS Algorithm code redone due to misunderstanding of concept
    Fully works fine now according to FCFS results match up perfectly

--- FCFS/FCFS/FCFS.cpp
+++ FCFS/FCFS/FCFS.cpp
@@ -14,6 +14,7 @@ struct Process {												//A class called Process with a list of member varia
 	int process_number = 0;
 	int response_time = 0;
 	bool isDone = false;
+	vector <int> data ;
 };
 
 int Calc_Waiting_Time(int, int);
@@ -21,166 +22,136 @@ float Calc_CPU_Utilization(float, float);
 
 int main() {
 
-	int p[8][19] = { { 4,24,		5,73,	3,31,	5,27,	4,33,	6,43,	4,64,	5,19,	2 },				//Initialization of all processes in a 2-D Array
-	{ 18,31,	19,35,	11,42,	18,43,	19,47,	18,43,	17,51,	19,32,	10 },
-	{ 6,18,		4,21,	7,19,	4,16,	5,29,	7,21,	8,22,	6,24,	5 },
-	{ 17,42,	19,55,	20,54,	17,52,	15,67,	12,72,	15,66,	14 },
-	{ 5,81,		4,82,	5,71,	3,61,	5,62,	4,51,	3,77,	4,61,	3,42,	5 },
-	{ 10,35,	12,41,	14,33,	11,32,	15,41,	13,29,	11 },
-	{ 21,51,	23,53,	24,61,	22,31,	21,43,	20 },
-	{ 11,52,	14,42,	15,31,	17,21,	16,43,	12,31,	13,32,	15 }
+	int data[8][19] = { { 4,24,		5,73,	3,31,	5,27,	4,33,	6,43,	4,64,	5,19,	2 },				//Initialization of all processes in a 2-D Array
+						{ 18,31,	19,35,	11,42,	18,43,	19,47,	18,43,	17,51,	19,32,	10 },
+						{ 6,18,		4,21,	7,19,	4,16,	5,29,	7,21,	8,22,	6,24,	5 },
+						{ 17,42,	19,55,	20,54,	17,52,	15,67,	12,72,	15,66,	14 },
+						{ 5,81,		4,82,	5,71,	3,61,	5,62,	4,51,	3,77,	4,61,	3,42,	5 },
+						{ 10,35,	12,41,	14,33,	11,32,	15,41,	13,29,	11 },
+						{ 21,51,	23,53,	24,61,	22,31,	21,43,	20 },
+						{ 11,52,	14,42,	15,31,	17,21,	16,43,	12,31,	13,32,	15 }
 	};
 
 	vector <Process> vProcess;
+	vector <Process> ReadyQ;
 	vector <Process> doneProcess;
 
 	struct Process P[8];					//Declaration of an Object of an array of Process called "P"
 	struct Process tempProcess;
 
-	int last_CPU_endtime;
+	int last_CPU_endtime = 0;
 	int next_up;
 	int position;
 	int size;
 	int index;
+	int time = 0;
 	int counter = 0;
 	int total_waiting = 0;
 	int total_turnaround = 0;
 	int total_response = 0;
-	float total_CPU_Bursts;
+	float total_CPU_Bursts = 0;
 	float time_completion;
 	bool skip = false;
 
+	//Read in data and put it in the Process
+	for (signed int i = 0; i < 8; i++) {
+		//Assign Process Number
+		P[i].process_number = i;
+		for (signed int j = 18; j >= 0; j--) {
+			//Store data into the Process'
+			if (data[i][j] != 0)
+				P[i].data.push_back(data[i][j]);
+		}
+	}
+
 	//Initial iteration
-	for (int i = 0; i < 1; i++) {
-		if (i % 2 == 0) {
-			for (int j = 0; j < 8; j++) {
-				if (i == 0 && j == 0) {
-					P[0].time_CPU += p[0][0];
-					total_CPU_Bursts = (float) p[0][0];
-					P[0].time_IO = P[0].time_CPU + p[0][1];
-					P[0].end_time = P[0].time_IO;
-					P[0].process_number = 0;
-					vProcess.push_back(P[0]);
-				}
-				else {
-					P[j].CPU_Burst = p[j][i];
-					total_CPU_Bursts += p[j][i];
-					P[j].IO_Burst = p[j][i + 1];
-					P[j].process_number = j;
-					if (p[j][i] == 0)
-						P[j].IO_Burst = 0;
-					else {
-						P[j].start_time = P[j - 1].time_CPU;
-						P[j].response_time = P[j].waiting_time += Calc_Waiting_Time(P[j].start_time, P[j].end_time);
-						P[j].time_CPU = P[j].start_time + P[j].CPU_Burst;
-						P[j].time_IO = P[j].time_CPU + P[j].IO_Burst;
-						P[j].end_time = P[j].time_IO;
-						vProcess.push_back(P[j]);
-						last_CPU_endtime = P[j].time_CPU;
-					}
-				}
-			}
-		}
+	P[0].CPU_Burst = P[0].data.back();
+	P[0].data.pop_back();
+	total_CPU_Bursts += P[0].CPU_Burst;
+	P[0].IO_Burst = P[0].data.back();
+	P[0].data.pop_back();
+	P[0].start_time = time;
+	P[0].response_time = P[0].waiting_time = 0;
+	P[0].time_CPU = P[0].start_time + P[0].CPU_Burst;
+	P[0].end_time = P[0].time_IO = P[0].time_CPU + P[0].IO_Burst;
+	vProcess.push_back(P[0]);
+	for (unsigned int j = 1; j < 8; j++) {
+		P[j].CPU_Burst = P[j].data.back();
+		P[j].data.pop_back();
+		total_CPU_Bursts += P[j].CPU_Burst;
+		P[j].IO_Burst = P[j].data.back();
+		P[j].data.pop_back();
+		P[j].start_time = P[j - 1].time_CPU;
+		P[j].response_time = P[j].waiting_time += Calc_Waiting_Time(P[j].start_time, P[j].end_time);
+		time = P[j].time_CPU = P[j].start_time + P[j].CPU_Burst;
+		P[j].time_IO = P[j].time_CPU + P[j].IO_Burst;
+		P[j].end_time = P[j].time_IO;
+		vProcess.push_back(P[j]);
 	}
 
-	//Sorts Process 
-	size = vProcess.size();
-	for (unsigned int k = 0; k < vProcess.size(); k++) {
-		//find the smallest io in vProcess
-		position = 0;
+	//Main Iteration
+	while (doneProcess.size() != 8) {
+
+		//Find next_up
 		next_up = vProcess[0].end_time;
-		for (signed int a = 1; a < size; a++) {
-			if (vProcess[a].end_time < next_up) {
-				next_up = vProcess[a].end_time;
-				position = a;
+		position = 0;
+		for (unsigned int i = 1; i < vProcess.size(); i++) {
+			if (vProcess[i].end_time < next_up) {
+				next_up = vProcess[i].end_time;
+				position = i;
 			}
 		}
-		//Use tempProcess to copy vProcess[position] essentially moving it to the back
-		tempProcess = vProcess[position];
+
+		//Delete from vProcess and then Push it to ReadyQ, like we were to move content
+		ReadyQ.push_back(vProcess[position]);
 		vProcess.erase(vProcess.begin() + position);
-		vProcess.push_back(tempProcess);
-		//size-- so that we ignore what we just pushed to the back iterating all the processes from previous ReadyQ
-		size--;
+
+		//Execute ReadyQ
+		for (unsigned int j = 0; j < ReadyQ.size(); j++) {
+			//If process is still in IO time when it is Ready to Execute, then it will start after it finishes it's IO
+				total_CPU_Bursts += ReadyQ[j].CPU_Burst = ReadyQ[j].data.back();
+				ReadyQ[j].data.pop_back();
+				if (ReadyQ[j].data.empty() == true) {
+					ReadyQ[j].isDone = true;
+					ReadyQ[j].IO_Burst = 0;
+					ReadyQ[j].start_time = time;
+					if (ReadyQ[j].start_time < ReadyQ[j].end_time)
+						ReadyQ[j].start_time = ReadyQ[j].end_time;
+					ReadyQ[j].waiting_time += Calc_Waiting_Time(ReadyQ[j].start_time, ReadyQ[j].end_time);
+					ReadyQ[j].time_CPU = ReadyQ[j].start_time + ReadyQ[j].CPU_Burst;
+					ReadyQ[j].time_IO = ReadyQ[j].time_CPU + ReadyQ[j].IO_Burst;
+					ReadyQ[j].end_time = ReadyQ[j].time_IO;
+					time = last_CPU_endtime = ReadyQ[j].time_CPU;
+					vProcess.push_back(ReadyQ[j]);
+				}
+				else {
+					ReadyQ[j].IO_Burst = ReadyQ[j].data.back();
+					ReadyQ[j].data.pop_back();
+					ReadyQ[j].start_time = time;
+					if (ReadyQ[j].start_time < ReadyQ[j].end_time)
+						ReadyQ[j].start_time = ReadyQ[j].end_time;
+					ReadyQ[j].waiting_time += Calc_Waiting_Time(ReadyQ[j].start_time, ReadyQ[j].end_time);
+					ReadyQ[j].time_CPU = ReadyQ[j].start_time + ReadyQ[j].CPU_Burst;
+					ReadyQ[j].time_IO = ReadyQ[j].time_CPU + ReadyQ[j].IO_Burst;
+					ReadyQ[j].end_time = ReadyQ[j].time_IO;
+					time = last_CPU_endtime = ReadyQ[j].time_CPU;
+					vProcess.push_back(ReadyQ[j]);
+				}
 	}
 
-	//Main iteration
-	for (unsigned int i = 2; i < 19; i++) {
-		size = vProcess.size();
-		if (i % 2 == 0) {
-			//Put CPU and IO Burst Times in it's respective Process
-			for (unsigned int j = 0; j < 8; j++) {
-				index = 0;
-				while (vProcess[index].process_number != P[j].process_number) {
-					if (index == vProcess.size() - 1) {
-						skip = true;
-						break;
-					}
-					else
-						index++;
-				}
-				if (skip == true)
-					//do nothing
-					skip = false;
-				else {
-					vProcess[index].CPU_Burst = P[j].CPU_Burst = p[j][i];
-					total_CPU_Bursts += p[j][i];
-					if (i + 1 != 19) {
-						vProcess[index].IO_Burst = P[j].IO_Burst = p[j][i + 1];
-						if (vProcess[index].IO_Burst == 0) {
-							vProcess[index].isDone = true;
-						}
-					}
-					else {
-						vProcess[index].isDone = true;
-						vProcess[index].IO_Burst = 0;
-					}
-				}
-			}
+	//Clear ReadyQ
+	ReadyQ.clear();
 
-			//Execute Process according to the sort
-			for (unsigned int j = 0; j < vProcess.size(); j++) {
-				//If process is still in IO time when it is Ready to Execute, then it will start after it finishes it's IO
-				vProcess[j].start_time = last_CPU_endtime;
-				if (vProcess[j].start_time < vProcess[j].end_time)
-					vProcess[j].start_time = vProcess[j].end_time;
-				vProcess[j].waiting_time += Calc_Waiting_Time(vProcess[j].start_time, vProcess[j].end_time);
-				vProcess[j].time_CPU = vProcess[j].start_time + vProcess[j].CPU_Burst;
-				vProcess[j].time_IO = vProcess[j].time_CPU + vProcess[j].IO_Burst;
-				vProcess[j].end_time = vProcess[j].time_IO;
-				last_CPU_endtime = vProcess[j].time_CPU;
-			}
-
-			//Check which process is done executing all its data
-			for (unsigned int j = 0; j < vProcess.size(); j++) {
-				if (vProcess[j].isDone == true) {
-					doneProcess.push_back(vProcess[j]);
-					vProcess.erase(vProcess.begin() + j);
-					//Starts at position where you might have deleted a Process
-					j--;
-				}
-			}
-
-			size = vProcess.size();
-
-			//Sorts
-			for (unsigned int j = 0; j < vProcess.size(); j++) {
-				//find the smallest io in vProcess
-				position = 0;
-				next_up = vProcess[0].end_time;
-				for (signed int a = 1; a < size; a++)
-					if (vProcess[a].end_time < next_up) {
-						next_up = vProcess[a].end_time;
-						position = a;
-					}
-				//Use tempProcess to copy vProcess[position] essentially moving it to the back
-				tempProcess = vProcess[position];
-				vProcess.erase(vProcess.begin() + position);
-				vProcess.push_back(tempProcess);
-				//size-- so that we ignore what we just pushed to the back iterating all the processes from previous ReadyQ
-				size--;
-			}
+	//Check which process is done executing all its data
+	for (unsigned int j = 0; j < vProcess.size(); j++) {
+		if (vProcess[j].isDone == true) {
+			doneProcess.push_back(vProcess[j]);
+			vProcess.erase(vProcess.begin() + j);
+			//Starts at position where you might have deleted a Process
+			j--;
 		}
 	}
+}
 
 	cout << "\t\t\tFCFS Scheduling Algorithm\n";
 	cout << "\tWaiting Time\tTurnaround Time\t\tResponse Time" << endl;
@@ -195,6 +166,7 @@ int main() {
 		cout << "\t\t\t" << doneProcess[index].response_time << "ms\n";
 	}
 
+	
 	time_completion = (float) doneProcess[0].end_time;
 	total_waiting += doneProcess[0].waiting_time;
 	total_turnaround += doneProcess[0].end_time;
@@ -207,10 +179,10 @@ int main() {
 			time_completion = (float) doneProcess[i].end_time;
 	}
 
-	cout << "Avg:\t" << (float)total_waiting / 8 << "ms\t\t" << (float)total_turnaround / 8 << "ms\t\t" << (float)total_response / 8 << "ms\n";
+	cout << "Avg:\t" << (float)total_waiting / 8 << "ms\t" << (float)total_turnaround / 8 << "ms\t\t\t" << (float)total_response / 8 << "ms\n";
 	cout << "FCFS CPU Utilization: " << setprecision(4) << Calc_CPU_Utilization(total_CPU_Bursts, time_completion) << "%" << endl;
 	counter = 0;
-
+	
 	return 0;
 }
 
